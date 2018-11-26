26cd91812993e37602087d4982f471d16da5847d
bendspoons <bendspoons>
2018-11-26 12:17:55 +0100

    Added java File Start contents

---
+++ src/android/Pdf2png.java
@@ -0,0 +1,20 @@
+package org.apache.cordova.plugin;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Pdf2png extends CordovaPlugin {
+
+  @Override
+  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+      if (action.equals("getPage")) {
+          callbackContext.success("ToDo");
+          return true;
+      }
+      return false;
+  }
+}
