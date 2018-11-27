6cbea63e1904a915d5a8bab2857d68eb568cec64
bendspoons <dominic.roesmann@googlemail.com>
2018-11-27 20:50:44 +0100

    Android plugin developemnt 90%

--- www/Pdf2png.js
+++ www/Pdf2png.js
@@ -4,25 +4,47 @@ function Pdf2png() {}
 
 
 Pdf2png.prototype.getPage = function(obj){
-        var cD=new Date();    
-        exec(
-            function(reply){ obj.success(reply); },
-            function(err){ obj.fail('Error: '+err); }
-        , "Pdf2png", "getPage", [obj.pdf,obj.page,obj.width,obj.height,obj.autoRelease]);
+  exec(function(reply){
+    obj.success(reply);
+  }, function(err){
+    obj.fail(err);
+  }, "Pdf2png", "getPage", [
+    obj.pdf,
+    obj.page,
+    obj.width,
+    obj.height,
+    obj.autoRelease
+  ]);
 };
+
+Pdf2png.prototype.countPages = function(obj){
+  exec(function(reply){
+    obj.success(reply);
+  }, function(err){
+    obj.fail(err);
+  }, "Pdf2png", "countPages", [
+    obj.pdf
+  ]);
+};
+
+
+Pdf2png.prototype.closePDF = function(obj){
+  exec(function(reply){
+    obj.success(reply);
+  }, function(err){
+    obj.fail(err);
+  }, "Pdf2png", "closePDF", [
+
+  ]);
+};
+
+
 
 Pdf2png.prototype.getPageInForeground = function(obj){
         exec(
             function(reply){ callback(reply); },
             function(err){ callback('Error: '+err); }
-        , "Pdf2png", "getPageInForeground", [obj.pdf,obj.page,obj.width,obj.height,obj.autoRelease]);
-};
-
-Pdf2png.prototype.closePDF = function(success,fail){
-        exec(
-            function(reply){ success(reply); },
-            function(err){ fail('Error: '+err); }
-        , "Pdf2png", "closePDF", []);
+        , "Pdf2png", "getPageInForeground", [obj.pdf,obj.page,obj.width,obj.height,obj.autoRelease,obj.targetDirectory]);
 };
 
 
